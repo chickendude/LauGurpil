52fa7205d6bf0f8369f6c5931f7ed958a0e7e3b8
chickendude <pokemaster103@gmail.com>
2021-09-23 17:40:43 -0400

    Add acceleration and momentum

--- source/main.c
+++ source/main.c
@@ -29,25 +29,49 @@ int main(void)
     oam_copy(oam_mem, obj_buffer, 128);
 
     int i = 0;
-    int angle = 0;
+    int angle = 0x8800;
     int dx, dy;
+    int sx, sy; // slide values
+    dx = lu_sin(angle);
+    dy = lu_cos(angle);
+    sx = dx;
+    sy = dy;
     int speed = 0;
     signed int x, y;
-    x = y = 80;
+    x = y = 0;
     while (i >= 0)
     {
         vid_vsync();
         oam_copy(oam_mem, obj_buffer, 128);
 
         key_poll();
-        angle -= key_tri_horz() * 0x0200;
-        speed = key_tri_vert();
-        dx = speed * lu_sin(angle);
-        dy = speed * lu_cos(angle);
-        x += dx;
-        y += dy;
+        angle -= key_tri_horz() * 0x0100; // Car turning power
+        speed += key_tri_vert() * 0x0050; // Acceleration power
+        if (speed > 0x2000) speed = 0x2000; // Max speed
+        if (speed < -0x2000) speed = -0x2000;
 
-        obj_set_pos(&obj_buffer[0], x >> 12, y >> 12);
+        // Calculate velocity
+        dx = lu_sin(angle);
+        dy = lu_cos(angle);
+        sx = (dx + 31 * sx) >> 5;
+        sy = (dy + 31 * sy) >> 5;
+        dx = (dx + sx) >> 1;
+        dy = (dy + sy) >> 1;
+        x += (speed * dx) >> 12;
+        y += (speed * dy) >> 12;
+
+        //
+        if (key_is_up(KEY_UP) && speed < 0)
+        {
+            speed += 0x0025;
+            if (speed > 0) speed = 0;
+        }
+        if (key_is_up(KEY_DOWN) && speed > 0) {
+            speed -= 0x0025;
+            if (speed < 0) speed = 0;
+        }
+
+        obj_set_pos(&obj_buffer[0], (x >> 12) - 8, (y >> 12) - 8);
         obj_aff_identity(&obj_aff_buffer[0]);
         obj_aff_rotate(&obj_aff_buffer[0], angle);
     }
