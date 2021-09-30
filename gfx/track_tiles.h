cb4ec7aa3d586a6f4e86f68e1ca55bcf8bc2269b
chickendude <pokemaster103@gmail.com>
2021-09-30 18:41:09 -0400

    Add tire sprite and update track

--- gfx/track_tiles.h
+++ gfx/track_tiles.h
@@ -3,12 +3,12 @@
 
 //======================================================================
 //
-//	track_tiles, 128x112@4, 
+//	track_tiles, 256x80@4, 
 //	+ palette 256 entries, not compressed
-//	+ 224 tiles Metatiled by 2x2 not compressed
-//	Total size: 512 + 7168 = 7680
+//	+ 320 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 10240 = 10752
 //
-//	Time-stamp: 2021-09-28, 19:36:33
+//	Time-stamp: 2021-09-30, 18:39:51
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
 //	( http://www.coranac.com/projects/#grit )
 //
@@ -17,8 +17,8 @@
 #ifndef GRIT_TRACK_TILES_H
 #define GRIT_TRACK_TILES_H
 
-#define track_tilesTilesLen 7168
-extern const unsigned int track_tilesTiles[1792];
+#define track_tilesTilesLen 10240
+extern const unsigned int track_tilesTiles[2560];
 
 #define track_tilesPalLen 512
 extern const unsigned short track_tilesPal[256];
--- gfx/track_tiles.tsx
+++ gfx/track_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="track_tiles" tilewidth="16" tileheight="16" tilecount="64" columns="8">
- <image source="track_tiles.png" width="128" height="128"/>
+<tileset version="1.5" tiledversion="1.7.2" name="track_tiles" tilewidth="16" tileheight="16" tilecount="80" columns="16">
+ <image source="track_tiles.png" width="256" height="80"/>
 </tileset>
