06ea93cbd9214962950e519dca27c6f9cef8fb60
chickendude <pokemaster103@gmail.com>
2021-10-12 17:40:40 -0400

    Add documentation

--- include/state.h
+++ include/state.h
@@ -16,8 +16,26 @@ struct StateStack
     State *states[10];
 };
 
+/**
+ * Pushes a new state onto the state stack.
+ *
+ * You can optionally provide a parameter as a pointer which will be passed to
+ * the new state.
+ *
+ * @param state_stack The stack to push the state onto.
+ * @param state The state to push onto the stack.
+ * @param parameter A pointer to some data to share between the two states.
+ */
 void push_state(StateStack *state_stack, State *state, void *parameter);
 
+/**
+ * Pop the current state off the stack and load the previous state.
+ *
+ * If need be, you can pass a pointer back to the previous state.
+ *
+ * @param state_stack The stack to push the state onto.
+ * @param parameter A pointer to some data to pass back to previous stack item.
+ */
 void pop_state(StateStack *state_stack, void *parameter);
 
 #endif //TEST_STATE_H
